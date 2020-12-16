b6ec523279b38b72e2db3508f046971e6c92538f
diem <dm.sakhonchik@gmail.com>
2020-12-16 16:17:26 +0300

    add factory

---
+++ src/main/java/by/sakhonchik/task01/service/ApplianceFactory.java
@@ -0,0 +1,28 @@
+package by.sakhonchik.task01.service;
+
+import by.sakhonchik.task01.entity.*;
+import by.sakhonchik.task01.entity.criteria.Criteria;
+
+public class ApplianceFactory {
+
+    public Appliance getAppliance(Criteria criteria) {
+        String nameOfAppliance = criteria.getGroupSearchName();
+
+        switch (nameOfAppliance) {
+            case "Oven":
+                return new Oven();
+            case "Laptop":
+                return new Laptop();
+            case "Refrigerator":
+                return new Refrigerator();
+            case "Speakers":
+                return new Speakers();
+            case "TabletPC":
+                return new TabletPC();
+            case "VacuumCleaner":
+                return new VacuumCleaner();
+            default:
+                return null;
+        }
+    }
+}
